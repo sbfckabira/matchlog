5d8e3cb2168ce52847718de9a5283967d676323d
川平朝章 <tomokabira@kabirachoushounoMacBook-Pro.local>
2021-08-21 07:34:14 +0900

    rails generateの自動生成ファイルを設定

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,10 @@
-require_relative 'boot'
+# require_relative 'boot'
 
-require 'rails/all'
+# require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# Bundler.require(*Rails.groups)
 
 module Matchlog
   class Application < Rails::Application
